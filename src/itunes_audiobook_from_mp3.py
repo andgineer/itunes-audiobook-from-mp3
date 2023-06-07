b0f3872de7f85fbc489158ed3e8c55f01c281979
andgineer <Andrei_Sorokin2@epam.com>
2023-06-07 06:12:05 +0200

    https://github.com/andgineer/itunes-audiobook-from-mp3/issues/4 black mypy

--- src/itunes_audiobook_from_mp3.py
+++ src/itunes_audiobook_from_mp3.py
@@ -1,67 +1,72 @@
+import os
+from argparse import ArgumentParser, Namespace
+
 import eyed3
 import eyed3.id3
-import os
-from argparse import ArgumentParser
+
+OPT_ENCODING_NO_ENCODING = "none"
+OPT_TRACK_NUM_NO_TRACK_NUM = "none"
+OPT_TRACK_NUM_BY_FILE_NAMES = ""
 
 
-OPT_ENCODING_NO_ENCODING = 'none'
-OPT_TRACK_NUM_NO_TRACK_NUM = 'none'
-OPT_TRACK_NUM_BY_FILE_NAMES = ''
-
-
-def fix_encoding(s):
-    if opts.encoding.lower() != OPT_ENCODING_NO_ENCODING.lower():
+def fix_encoding(text: str, fix_encoding: str) -> str:
+    """Decode from specifyed in CLI encoding."""
+    if fix_encoding.lower() != OPT_ENCODING_NO_ENCODING.lower():
         try:
-            s = s.encode('latin_1').decode(opts.encoding)
+            text = text.encode("latin_1").decode(fix_encoding)
         except UnicodeEncodeError as e:
-            print(s)
+            print(text)
             print(e)
-    return s
+    return text
 
 
-def get_opts():
-    parser = ArgumentParser(description='Fixes mp3 tags for iOS audiobooks.')
-    parser.add_argument('folder', default='.', metavar='folder', nargs='?', help='Folder to process')
+def get_opts() -> Namespace:
+    """Get CLI options."""
+    parser = ArgumentParser(description="Fixes mp3 tags for iOS audiobooks.")
     parser.add_argument(
-        '--encoding',
-        default='cp1251',
-        dest='encoding',
+        "folder", default=".", metavar="folder", nargs="?", help="Folder to process"
+    )
+    parser.add_argument(
+        "--encoding",
+        default="cp1251",
+        dest="encoding",
         help=f'mp3 tags encoding. "{OPT_ENCODING_NO_ENCODING}" if you do not need mp3 tags encoding fix.',
     )
-    parser.add_argument('--extension', default='mp3', dest='extension', help='File''s extension.')
+    parser.add_argument("--extension", default="mp3", dest="extension", help="File" "s extension.")
     parser.add_argument(
-        '--set-tag',
+        "--set-tag",
         default=None,
-        dest='set_tag',
-        nargs='*',
-        help='Change mp3 tag to specified string. Format "tag-name/tag-value".'
+        dest="set_tag",
+        nargs="*",
+        help='Change mp3 tag to specified string. Format "tag-name/tag-value".',
     )
     parser.add_argument(
-        '--track-num',
-        default='sort-file-names',
-        dest='track_num',
-        help=('''Set mp3 tag `track_num` as specified.
-                                - {} - file number for files sorted by names.
-                            Use {} if you do not want to set this tag.'''.format(
-                                OPT_TRACK_NUM_BY_FILE_NAMES,
-                                OPT_TRACK_NUM_NO_TRACK_NUM))
+        "--track-num",
+        default="sort-file-names",
+        dest="track_num",
+        help=(
+            f"""Set mp3 tag `track_num` as specified.
+            - {OPT_TRACK_NUM_BY_FILE_NAMES} - file number for files sorted by names.
+            Use {OPT_TRACK_NUM_NO_TRACK_NUM} if you do not want to set this tag."""
+        ),
     )
     parser.add_argument(
-        '--title-prefix',
-        default='{track:04} - ',
-        dest='title_prefix',
-        help='Prefix each file title with the track number for the file.'
+        "--title-prefix",
+        default="{track:04} - ",
+        dest="title_prefix",
+        help="Prefix each file title with the track number for the file.",
     )
-    parser.add_argument('--dry', dest='dry', help='Just dry run without files fix.')
+    parser.add_argument("--dry", dest="dry", help="Just dry run without files fix.")
     opts, _ = parser.parse_known_args()
     opts.set_tags = {}
     if opts.set_tag:
         for tag_string in opts.set_tag:
-            opts.set_tags.update({tag_string.split('/')[0]: tag_string.split('/')[1]})
+            opts.set_tags.update({tag_string.split("/")[0]: tag_string.split("/")[1]})
     return opts
 
 
-def fix_mp3_tags():
+def fix_mp3_tags(opts: Namespace) -> None:
+    """Fix mp3 tags."""
     paths = []
     for subdir, dirs, files in os.walk(opts.folder):
         for file_name in files:
@@ -73,40 +78,45 @@ def fix_mp3_tags():
 
     track = 1
     for file_path in paths:
-        if file_path.endswith('.' + opts.extension):
+        if file_path.endswith("." + opts.extension):
             print(file_path)
             audio_file = eyed3.load(file_path)
-            for tag_name in ['artist', 'title', 'album', 'album_artist']:
+            for tag_name in ["artist", "title", "album", "album_artist"]:
                 if tag_name in opts.set_tags:
-                    audio_file.tag.__setattr__(tag_name, opts.set_tags[tag_name])
+                    setattr(audio_file.tag, tag_name, opts.set_tags[tag_name])
                 else:
-                    if audio_file.tag.__getattribute__(tag_name):
-                        audio_file.tag.__setattr__(tag_name, fix_encoding(audio_file.tag.__getattribute__(tag_name)))
-            audio_file.tag.genre = 'Audiobook'
-            audio_file.tag.mediatype = 'Audiobook'
-            #audio_file.tag.compilation = '1'
-            #audiofile.tag.images.set(3, open('cover.jpg','rb').read(), 'image/jpeg')
+                    if getattr(audio_file.tag, tag_name):
+                        setattr(
+                            audio_file.tag,
+                            tag_name,
+                            fix_encoding(getattr(audio_file.tag, tag_name), opts.encoding),
+                        )
+            audio_file.tag.genre = "Audiobook"
+            audio_file.tag.mediatype = "Audiobook"
+            # audio_file.tag.compilation = '1'
+            # audiofile.tag.images.set(3, open('cover.jpg','rb').read(), 'image/jpeg')
             if opts.track_num:
                 audio_file.tag.track_num = track
-                audio_file.tag.part = '%04d' % track
+                audio_file.tag.part = f"{track:04d}"
                 audio_file.tag.chapter = track
-                audio_file.tag.title = (opts.title_prefix + '{name}').format(
-                    name=audio_file.tag.title,
-                    track=track)
+                audio_file.tag.title = (opts.title_prefix + "{name}").format(
+                    name=audio_file.tag.title, track=track
+                )
                 track += 1
-            print('{} by {}, album {} by {}, genre {}, part {}'.format(
-                audio_file.tag.title, audio_file.tag.artist, audio_file.tag.album, audio_file.tag.album_artist,
-                audio_file.tag.genre, audio_file.tag.part))
+            print(
+                f"{audio_file.tag.title} by {audio_file.tag.artist}, "
+                f"album {audio_file.tag.album} by {audio_file.tag.album_artist}, "
+                f"genre {audio_file.tag.genre}, part {audio_file.tag.part}"
+            )
             if not opts.dry:
-                audio_file.tag.save(version=eyed3.id3.ID3_DEFAULT_VERSION, encoding='utf-8')
+                audio_file.tag.save(version=eyed3.id3.ID3_DEFAULT_VERSION, encoding="utf-8")
 
 
-def main():
-    global opts
-
+def main() -> None:
+    """Do the work."""
     opts = get_opts()
-    fix_mp3_tags()
+    fix_mp3_tags(opts)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,37 +1,36 @@
-import pytest
 from unittest.mock import Mock, patch
-from itunes_audiobook_from_mp3 import fix_encoding, get_opts, fix_mp3_tags
+
+import pytest
+
+from itunes_audiobook_from_mp3 import fix_encoding, fix_mp3_tags
+
 
 def test_fix_encoding():
-    with patch('itunes_audiobook_from_mp3.opts', Mock(encoding="cp1251")):
-        assert fix_encoding("abc") == "abc"
+    assert fix_encoding("abc", fix_encoding="cp1251") == "abc"
 
-    with patch('itunes_audiobook_from_mp3.opts', Mock(encoding="none")):
-        assert fix_encoding("abc") == "abc"
+    assert fix_encoding("abc", fix_encoding="none") == "abc"
 
     with pytest.raises(LookupError):
-        with patch('itunes_audiobook_from_mp3.opts', Mock(encoding="invalid_encoding")):
-            fix_encoding("abc")
+        fix_encoding("abc", fix_encoding="invalid_encoding")
 
-@patch('itunes_audiobook_from_mp3.eyed3.load')
-@patch('os.walk')
+
+@patch("itunes_audiobook_from_mp3.eyed3.load")
+@patch("os.walk")
 def test_fix_mp3_tags(mock_os_walk, mock_eyed3_load):
-    with patch('itunes_audiobook_from_mp3.opts', Mock(
+    mock_os_walk.return_value = [("dir", [], ["file1.mp3"])]
+    mock_audio_file = Mock()
+    tag = Mock(artist="Artist", title="Title", album="Album", album_artist="Album Artist")
+    mock_audio_file.tag = tag
+    mock_eyed3_load.return_value = mock_audio_file
+    fix_mp3_tags(
+        Mock(
             folder="test_folder",
             encoding="cp1251",
             extension="mp3",
             set_tags={"tag1": "value1"},
             track_num="sort-file-names",
             title_prefix="test_prefix - ",
-            dry=True
-        )):
-        mock_os_walk.return_value = [("dir", [], ["file1.mp3"])]
-        mock_audio_file = Mock()
-        tag = Mock(artist='Artist', title='Title', album='Album', album_artist='Album Artist')
-        mock_audio_file.tag = tag
-        mock_eyed3_load.return_value = mock_audio_file
-        fix_mp3_tags()
-        assert mock_audio_file.tag.artist
-
-
-
+            dry=True,
+        )
+    )
+    assert mock_audio_file.tag.artist
