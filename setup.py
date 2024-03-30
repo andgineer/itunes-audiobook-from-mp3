f1d128de5b6e6cf626ce0ded3e52f56fd3f50bc1
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-03-30 10:09:45 +0100

    read version from file

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import codecs
+import os
+
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as f:
@@ -6,9 +9,26 @@ with open("README.md", "r", encoding="utf-8") as f:
 with open("requirements.in") as f:
     requirements = f.read().splitlines()
 
+# Solution from https://packaging.python.org/guides/single-sourcing-package-version/
+def read(rel_path: str) -> str:
+    """Read file."""
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path: str) -> str:
+    """Parse version from file content."""
+    for line in read(rel_path).splitlines():
+        if line.startswith("VERSION"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
 
 setuptools.setup(
     name="audiobook-tags",
+    version=get_version("src/audiobook_tags/version.py"),
     author="Andrey Sorokin",
     author_email="andrey@sorokin.engineer",
     description="Fix mp3 tags to use in iTunes/iPhone audiobooks",
